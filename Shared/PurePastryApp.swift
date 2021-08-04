426738b2d26378194bbc0d56be99b57a6f3755ea
jowens02 <46910303+jowens02@users.noreply.github.com>
2021-08-04 11:42:46 -0500

    Initial Commit

---
+++ Shared/PurePastryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurePastryApp.swift
+//  Shared
+//
+//  Created by Jheanelle Owens on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct PurePastryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
