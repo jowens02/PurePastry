9ba103500db2dc95197fc6c5dc143371228bec19
jowens02 <46910303+jowens02@users.noreply.github.com>
2021-08-07 10:49:06 -0500

    Notification allowed w badge

--- Shared/StartView.swift
+++ Shared/StartView.swift
@@ -7,17 +7,40 @@
 
 import UserNotifications
 import SwiftUI
+import CoreLocation
 
 struct NotificationView: View {
     var body: some View {
         VStack {
             Button("Allow Notifications") {
+                UNUserNotificationCenter.current()
+                .requestAuthorization(options: [.alert, .badge, .sound]){ success, error in
+                        if success {
+                            print("SUCCESS")
+                        } else if let error = error {
+                            print(error.localizedDescription)
+                        }
+                    }
                 
             }
             
-            Button("Schedule Notofications") {
+            Button("Schedule Notifications") {
+                let content = UNMutableNotificationContent()
+                content.title = "Sweets Nearby!"
+                content.subtitle = "Check the map to see how close you are!"
+                content.sound = UNNotificationSound.default
+                content.badge = 1
                 
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
             }
+        }
+        
+        .onAppear() {
+            UIApplication.shared.applicationIconBadgeNumber = 0
         }
     }
 }
@@ -33,7 +56,7 @@ struct StartView: View {
                     .aspectRatio(
                         contentMode: .fit)
                     .offset(y: -60)
-                Text("Welcome to Pure Pastry Tracker")
+                Text("Welcome to Kim Gregory     Pure Pastry Tracker")
                     .font(.title)
                     .fontWeight(.semibold)
                     .multilineTextAlignment(.center)
@@ -42,7 +65,7 @@ struct StartView: View {
                     .background(Color.orange)
                     .offset(y: -60)
                 
-                Text("Thank you for supporting our business")
+                Text("Pure Pastry is Pure Love")
                     .fontWeight(.heavy)
                     .multilineTextAlignment(.center)
                     .offset(y: -60)
